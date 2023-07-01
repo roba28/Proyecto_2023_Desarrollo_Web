85958d4c4fbde57521346c4f7e7044a0b4d16516
Ronny <Ronny@ADSP-13207>
2023-07-01 17:48:26 -0600

    Carga Inicial

---
+++ src/main/java/Proyecto_2023_Desarrollo_Web/Proyecto2023DesarrolloWebApplication.java
@@ -0,0 +1,13 @@
+package Proyecto_2023_Desarrollo_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2023DesarrolloWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2023DesarrolloWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Proyecto_2023_Desarrollo_Web/Proyecto2023DesarrolloWebApplicationTests.java
@@ -0,0 +1,13 @@
+package Proyecto_2023_Desarrollo_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2023DesarrolloWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
